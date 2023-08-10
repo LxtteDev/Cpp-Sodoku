51238d9f70d173e79ac845c0854f7bf3cb9f6f79
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-08-10 16:36:55 -0700

    Default Board Generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+int board[9][9]; // X, Y
+
+void showBoard() {
+    for (unsigned int y = 0; y < 9; y++) {
+        if ((y) % 3 == 0 && y != 0) std::cout << std::endl << "-------|-------|-------";
+        for (unsigned int x = 0; x < 9; x++) {
+            if ((x) % 3 == 0 && x != 0) std::cout << " |";
+            if (x == 0) std::cout << std::endl;
+            std::cout << " " << board[x][y]; 
+        }
+    }
+}
+
+void generateBoard(float difficulty) {
+    // Create Default Board
+    for (unsigned int y = 0; y < 9; y++)
+        for (unsigned int x = 0; x < 9; x++)
+            board[x][y] = ((x + (y % 3 * 3)) + (y - y % 3) / 3) % 9 + 1;
+}
+
 int main(int, char**){
-    std::cout << "Hello, from sodoku!\n";
+    generateBoard(0.5f);
+    showBoard();
+
+    std::cout << std::endl << std::endl;
 }
